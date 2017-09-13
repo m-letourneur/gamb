565bf969c840f6de5b4151efb50e3ce51cc3f7f9
Marc Letourneur <marc@fibronostics.com>
2017-09-13 18:03:45 +0800

    pickle solved

--- model/build_model.py
+++ model/build_model.py
@@ -9,16 +9,18 @@ import pandas as pd
 from sklearn.preprocessing import normalize
 from sklearn.svm import LinearSVC
 from sklearn.metrics import confusion_matrix
-from pickle import dump
+from pickle import dump, load
 
 from modules.helper import get_games_in_season
 
+SAVE_FEATURES_OUTCOMES = True
+UNPACK_FEATURES_OUTCOMES = False
 
 if __name__ == '__main__':
 
     # List of seasons used to build the model
     # SEASONS = ['2013_2014', '2014_2015']
-    SEASONS = ['2016_2017']    
+    SEASONS = ['2016_2017']
     LEAGUE = 'E0'
 
     # Construct the features/outcomes
@@ -28,14 +30,35 @@ if __name__ == '__main__':
     outcomes = []
 
     league = LEAGUE
-    for season in SEASONS:
-        games_in_season, dates_dt = get_games_in_season(season, league)
-        for game, date_dt in list(zip(games_in_season, dates_dt)):
-            game_inst = g.Game()
-            game_inst(game, season, league, date_dt)
-            features.append(game_inst.features)
-            outcomes.append(game_inst.outcome)
-            # print 'in'
+
+    if UNPACK_FEATURES_OUTCOMES:
+        features_pkl = open(
+            basedir + '/stored_features/features.pkl', "r")  # Pickling
+        features = load(features_pkl)
+        outcomes_pkl = open(
+            basedir + '/stored_features/outcomes.pkl', "r")  # Pickling
+        outcomes = load(outcomes_pkl)
+        # features = pd.read_pickle(basedir + '/stored_features/features.p')
+    else:
+        for season in SEASONS:
+            games_in_season, dates_dt = get_games_in_season(season, league)
+            for game, date_dt in list(zip(games_in_season, dates_dt)):
+                game_inst = g.Game()
+                game_inst(game, season, league, date_dt)
+                features.append(game_inst.features)
+                outcomes.append(game_inst.outcome[0])
+                # print 'in'
+
+    if SAVE_FEATURES_OUTCOMES:
+        features_pkl = open(
+            basedir + '/stored_features/features.pkl', "wb")  # Pickling
+        dump(features, features_pkl)
+        features_pkl.close()
+        outcomes_pkl = open(
+            basedir + '/stored_features/outcomes.pkl', "wb")  # Pickling
+        dump(outcomes, outcomes_pkl)
+        outcomes_pkl.close()
+        # features.to_pickle(basedir + '/stored_features/features.p')
 
     # features and outcomes can now feed/train the model
     # print outcomes
@@ -49,8 +72,9 @@ if __name__ == '__main__':
     learner = LinearSVC()
     learner.fit(features_normalized, outcomes)
     out_file = open(basedir + '/stored_models/linearSVC_' +
-                    LEAGUE + '.p', 'wr')
+                    LEAGUE + '.pkl', 'wb')
     dump(learner, out_file)
+    out_file.close()
 
     # Training score
     print learner.score(features_normalized, outcomes)
--- model/validate_model.py
+++ model/validate_model.py
@@ -9,22 +9,15 @@ from modules.helper import get_games_in_season
 import pandas as pd
 from sklearn.preprocessing import normalize
 from sklearn.svm import LinearSVC
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 from pickle import load
 
-
-# def _get_games_in_season(season, league):
-#     """
-#     List of games and associated list of dates in datetime format
-#     """
-#     games_in_season, dates_dt = [], []
-
-#     return games_in_season, dates_dt
+UNPACK_FEATURES_OUTCOMES = False
 
 if __name__ == '__main__':
 
     # List of seasons used to build the model
-    SEASONS = ['2016_2017']
+    SEASONS = ['2017_2018']
     LEAGUE = 'E0'
 
     # Construct the features/outcomes
@@ -33,14 +26,24 @@ if __name__ == '__main__':
     features = []
     outcomes = []
 
-    league = LEAGUE
-    for season in SEASONS:
-        games_in_season, dates_dt = get_games_in_season(season, league)
-        for game, date_dt in zip(games_in_season, dates_dt):
-            game_inst = g.Game()
-            game_inst(game, season, league, date_dt)
-            features.append(game_inst.features)
-            outcomes.append(game_inst.outcome)
+    if UNPACK_FEATURES_OUTCOMES:
+        features_pkl = open(
+            basedir + '/stored_features/features.pkl', "r")  # Pickling
+        features = load(features_pkl)
+        outcomes_pkl = open(
+            basedir + '/stored_features/outcomes.pkl', "r")  # Pickling
+        outcomes = load(outcomes_pkl)
+        # features = pd.read_pickle(basedir + '/stored_features/features.p')
+    else:
+        league = LEAGUE
+        for season in SEASONS:
+            games_in_season, dates_dt = get_games_in_season(season, league)
+            for game, date_dt in list(zip(games_in_season, dates_dt)):
+                game_inst = g.Game()
+                game_inst(game, season, league, date_dt)
+                features.append(game_inst.features)
+                outcomes.append(game_inst.outcome[0])
+                # print 'in'
 
     # features and outcomes can now feed/train the model
 
@@ -50,9 +53,12 @@ if __name__ == '__main__':
     features_normalized = features
     # Predicting using model
     p_file = open(basedir + '/stored_models/linearSVC_' +
-                  LEAGUE + '.p', 'rb')
+                  LEAGUE + '.pkl', 'rb')
     model = load(p_file)
-    predicted = model.fit(features_normalized)
-
+    predicted = model.predict(features_normalized)
+    print 'predicted = ' + str(predicted)
+    print '\noutcomes = ' + str(outcomes) + '\n'
     # Validation score
+    print model.score(features_normalized, outcomes)
     print confusion_matrix(outcomes, predicted)
+    print classification_report(outcomes, predicted, labels=['0','1'], target_names=['DL', 'W'])
